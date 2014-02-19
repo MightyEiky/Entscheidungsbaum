3421e25ed3aeede44e42855d3492a6f9c4574afa
JuliusReischauer <JReischauer@web.de>
2014-02-19 15:55:01 +0100

    Initial commit of JavaFXExample.java BUILD PATH: Added jfxrt.jar to classpath

---
+++ Entscheidungsbaum/src/ui/JavaFXExample.java
@@ -0,0 +1,16 @@
+package ui;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class JavaFXExample extends Application {
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		stage.show();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
