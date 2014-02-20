38a75c86086b0ee3dbb63c2736b48e08e9e6ee00
BaylifeBob <JReischauer@web.de>
2014-02-20 16:34:26 +0100

    Rename Main.java to JavaFXExample.java Moved files JavaFXExample.java, ApplicationController.java and Simple.fxml from package core to package ui

--- Entscheidungsbaum/src/ui/ApplicationController.java
+++ Entscheidungsbaum/src/ui/ApplicationController.java
@@ -1,4 +1,4 @@
-package core;
+package ui;
 
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
--- Entscheidungsbaum/src/ui/JavaFXExample.java
+++ Entscheidungsbaum/src/ui/JavaFXExample.java
@@ -1,16 +1,31 @@
 package ui;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class JavaFXExample extends Application {
 
-	@Override
-	public void start(Stage stage) throws Exception {
-		stage.show();
-	}
+    public static void main(String[] args) {
+        Application.launch(JavaFXExample.class, (java.lang.String[])null);
+    }
 
-	public static void main(String[] args) {
-		launch(args);
-	}
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            AnchorPane page = (AnchorPane) FXMLLoader.load(JavaFXExample.class.getResource("Simple.fxml"));
+            Scene scene = new Scene(page);
+            primaryStage.setScene(scene);
+            primaryStage.setTitle("Hello World Sample");
+            primaryStage.show();
+        } catch (Exception ex) {
+            Logger.getLogger(JavaFXExample.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
 }
