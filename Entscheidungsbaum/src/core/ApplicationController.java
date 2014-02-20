6c892c1b9a1294fc319824d9ef4220e830fda55c
BaylifeBob <JReischauer@web.de>
2014-02-20 16:30:40 +0100

    Added JavaFX example including files Simple.fxml, ApplicationController.java and changes to Main.java Added .txt-File explaining basics of JavaFX's xml-to-code relations

---
+++ Entscheidungsbaum/src/core/ApplicationController.java
@@ -0,0 +1,27 @@
+package core;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class ApplicationController {
+	
+	@FXML
+	private Button button2;
+	
+	@FXML
+	void initialize() {
+		button2.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent arg0) {
+				System.out.println("Hello World2");
+			}
+		});
+	}
+	
+	public void clickMe() {
+		System.out.println("Hello World");
+	}
+}
--- Entscheidungsbaum/src/core/Main.java
+++ Entscheidungsbaum/src/core/Main.java
@@ -1,13 +1,31 @@
 package core;
 
-public class Main {
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hallo Welt");
-	}
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        Application.launch(Main.class, (java.lang.String[])null);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            AnchorPane page = (AnchorPane) FXMLLoader.load(Main.class.getResource("Simple.fxml"));
+            Scene scene = new Scene(page);
+            primaryStage.setScene(scene);
+            primaryStage.setTitle("Hello World Sample");
+            primaryStage.show();
+        } catch (Exception ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 
 }
