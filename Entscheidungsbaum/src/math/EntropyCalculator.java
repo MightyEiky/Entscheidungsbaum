107e7cb848f46bbc24ff39f7d796a9295669121e
BaylifeBob <JReischauer@web.de>
2014-02-23 13:39:46 +0100

    Initial commit of EntropyCalculator.java and associated unit test EntropyCalculatorTest.java

---
+++ Entscheidungsbaum/src/math/EntropyCalculator.java
@@ -0,0 +1,33 @@
+package math;
+
+import java.util.Set;
+
+/**
+ * Calculates entropy.
+ * 
+ * @author Julius
+ * 
+ */
+public class EntropyCalculator {
+
+	/**
+	 * Calculates the entropy for a given Set of probabilities.
+	 * 
+	 * @param pProbabilities
+	 *            Set of probabilities
+	 * @return double containing the entropy, or -1 if the Set is empty
+	 */
+	public static double calculateEntropy(Set<Double> pProbabilities) {
+		double result = 0;
+
+		if (pProbabilities.size() == 0) {
+			return -1;
+		}
+
+		for (Double d : pProbabilities) {
+			result += (-1 * d) * Math.log10(d);
+		}
+
+		return result;
+	}
+}
---
+++ Entscheidungsbaum/unittest/math/EntropyCalculatorTest.java
@@ -0,0 +1,41 @@
+package math;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.Test;
+
+/**
+ * Testing EntropyCalculator class.
+ * 
+ * @author Julius
+ * 
+ */
+public class EntropyCalculatorTest {
+
+	/**
+	 * Testing calculateEntropy method of EntropyCalculator class.
+	 */
+	@Test
+	public void calculateEntropyTest() {
+		// testing method using empty Set
+		double minusOne = EntropyCalculator.calculateEntropy(new HashSet<Double>());
+		assertTrue("Expected -1, got " + minusOne + ".", minusOne == -1);
+
+		// testing method using regular parameters
+		double testValue1 = 6d / 11d;
+		double testValue2 = 5d / 11d;
+
+		double expected = (-1 * testValue1) * Math.log10(testValue1) + (-1 * testValue2) * Math.log10(testValue2);
+
+		Set<Double> params = new HashSet<Double>();
+		params.add(testValue1);
+		params.add(testValue2);
+
+		double actual = EntropyCalculator.calculateEntropy(params);
+
+		assertTrue("Expected " + expected + ", got " + actual + ".", expected == actual);
+	}
+}
